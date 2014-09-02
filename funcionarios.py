f75e6f208f56fe33867b073b278ef45b8f6462a5
Davi Souza <souza.davirs@gmail.com>
2014-09-02 09:03:25 -0300

    arquivo com nomes funcionarios a ser tratados

---
+++ funcionarios.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def lista_nomes():
+    funcionarios = ['Davi Roberto de Souza']
+                   #'Daniel Alves Paraíso',
+                   #'Giovanni Abraham Gobo']
+
+    return funcionarios
